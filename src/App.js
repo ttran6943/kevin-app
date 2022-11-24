5aec24d5d6799149d85cf9d4918993b013947381
Tony Tran <tran.tony.08@gmail.com>
2022-11-23 22:45:52 -0800

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const styleHeader = {
+  color: 'red',
+  lineHeight: 2,
+  textAlignLast: 'center'
+}
+
+const styleCount = {
+  color: 'blue',
+  textAlignLast: 'center'
+}
+
+const styleButtons = {
+  textAlignLast: 'center'
+}
+
+const marginButtons = {
+  margin: '20px'
+}
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>    
+      <h1 style={styleHeader}>How many times has Kevin accidentally called the groupchat?</h1>
+        <div>
+          <h2 style={styleCount}>{count}</h2>
+        </div>
+      <div style={styleButtons}>
+        <button  style={marginButtons} className="btn btn-primary" onClick={() => setCount(count + 1)}>
+          He did it again
+        </button>
+        <button style={marginButtons} className="btn btn-secondary" onClick={() => setCount(count - 1)}>
+          Whoops
+        </button>
+      </div>
+    </>
   );
 }
 
